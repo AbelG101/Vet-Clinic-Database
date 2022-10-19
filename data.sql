f8802819182169d4c71698d3aa409e1e1ba8b97c
AbelG101 <abelgebeyehu101@gmail.com>
2022-10-19 17:01:58 +0300

    Add species data

--- data.sql
+++ data.sql
@@ -21,3 +21,7 @@ VALUES ('sam smith', 34),
 	   ('Melody Pond ', 77),
 	   ('Dean Winchester ', 14),
 	   ('Jodie Whittaker ', 38);
+
+INSERT INTO species (name) 
+values ('Pokemon'),
+	   ('Digimon');
