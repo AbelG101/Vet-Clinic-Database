44dafb24c76e8d64f9d72529ca8a5ec6fa67e69a
AbelG101 <abelgebeyehu101@gmail.com>
2022-10-19 17:01:42 +0300

    Add owners data

--- data.sql
+++ data.sql
@@ -12,3 +12,12 @@ values ('Charmander','2020-02-08', 0, '1', -11),
 			 ('Boarmon','2005-06-07', 7, '1', 20.4),
 			 ('Blossom','1998-10-13', 3, '1', 17),
 			 ('Ditto','2022-05-14', 4, '1', 22);
+
+
+INSERT INTO owners (full_name, age) 
+VALUES ('sam smith', 34),
+	   ('Jennifer Orwell', 19),
+	   ('Bob', 45),
+	   ('Melody Pond ', 77),
+	   ('Dean Winchester ', 14),
+	   ('Jodie Whittaker ', 38);
